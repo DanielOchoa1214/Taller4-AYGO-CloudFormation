d098b91ea617640926f2b712a1de03b961791bf2
dano <danochoa1412@gmail.com>
2025-11-08 16:04:23 -0500

    Feature: removed app comments

--- src/main/java/com/myorg/HelloCdkApp.java
+++ src/main/java/com/myorg/HelloCdkApp.java
@@ -9,29 +9,10 @@ public class HelloCdkApp {
         App app = new App();
 
         new HelloCdkStack(app, "HelloCdkStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
-
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-
                 .env(Environment.builder()
                         .account(System.getenv("520046576100"))
                         .region(System.getenv("us-east-1"))
                         .build())
-
-
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                 .build());
 
         app.synth();
